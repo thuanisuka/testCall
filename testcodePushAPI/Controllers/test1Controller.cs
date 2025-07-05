557196000f173f74cb427de651ad2ecca57f99b3
ThuanLM <thuan13576%@gmail.com>
2025-07-05 19:29:42 +0700

    test code

---
+++ testcodePushAPI/Controllers/test1Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testcodePushAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class test1Controller : ControllerBase
+    {
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Simulate deletion logic
+            // In a real application, you would typically delete an item from a database or collection here.
+            // Return a 204 No Content response to indicate successful deletion
+            return NoContent();
+        }
+    }
+}
--- testcodePushAPI/obj/Debug/net8.0/testcodePushAPI.AssemblyInfo.cs
+++ testcodePushAPI/obj/Debug/net8.0/testcodePushAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testcodePushAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f471c835f2a21567faf18dad8624362c533d451f")]
 [assembly: System.Reflection.AssemblyProductAttribute("testcodePushAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testcodePushAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
