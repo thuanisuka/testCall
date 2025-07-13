366849f29b70d7119f0bc3e964d098ebbf442aa1
ThuanLM <thuan13576%@gmail.com>
2025-07-13 21:39:10 +0700

    co bug diii

---
+++ testcodePushAPI/Controllers/Hardcoded credentials (S2068).cs
@@ -0,0 +1,12 @@
+﻿namespace testcodePushAPI.Controllers
+{
+    public class Hardcoded_credentials__S2068_
+    {
+        public void ConnectToDb()
+        {
+            string password = "MySecretPassword123"; 
+            string connectionString = $"Server=myServer;Database=myDB;User Id=sa;Password={password};";
+            Console.WriteLine(connectionString);
+        }
+    }
+}
--- testcodePushAPI/obj/Debug/net8.0/testcodePushAPI.AssemblyInfo.cs
+++ testcodePushAPI/obj/Debug/net8.0/testcodePushAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testcodePushAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d06d07ecd3212001b37b71bdaf1de7f6dc767e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e51d6de64510a232ff83dc3eb0579ecbbd9953b")]
 [assembly: System.Reflection.AssemblyProductAttribute("testcodePushAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testcodePushAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
